bb232eca005564008be34581d567a4c66d9e5296
huadong.yang <huadong.yang@qizhidao.com>
2024-08-26 17:43:45 +0800

    feat：优化无对象读取

---
+++ src/main/java/birdsnail/example/excel/ExcelNoModelDataHandler.java
@@ -0,0 +1,71 @@
+package birdsnail.example.excel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.fastjson2.JSON;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+/**
+ * 不创建对象读取并处理excel
+ */
+public class ExcelNoModelDataHandler {
+
+    /**
+     * 处理excel数据
+     *
+     * @param fileName       excel
+     * @param recordsHandler 处理逻辑
+     */
+    public static void handleAll(String fileName, Consumer<List<Map<Integer, String>>> recordsHandler) {
+        EasyExcel.read(fileName, new NoModelDataListener(recordsHandler)).sheet().doRead();
+    }
+
+    @Slf4j
+    public static class NoModelDataListener extends AnalysisEventListener<Map<Integer, String>> {
+
+        /**
+         * 每隔5条存储数据库，实际使用中可以100条，然后清理list ，方便内存回收
+         */
+        private static final int BATCH_COUNT = 100;
+        private List<Map<Integer, String>> cachedDataList = new LinkedList<>();
+
+        private final Consumer<List<Map<Integer, String>>> recordsHandler;
+
+        public NoModelDataListener(Consumer<List<Map<Integer, String>>> recordsHandler) {
+            this.recordsHandler = recordsHandler;
+        }
+
+        @Override
+        public void invoke(Map<Integer, String> data, AnalysisContext context) {
+            log.info("解析到一条数据:{}", JSON.toJSONString(data));
+            cachedDataList.add(data);
+//            if (cachedDataList.size() >= BATCH_COUNT) {
+//                saveData();
+//                cachedDataList = ListUtils.newArrayListWithExpectedSize(BATCH_COUNT);
+//            }
+        }
+
+        @Override
+        public void doAfterAllAnalysed(AnalysisContext context) {
+            saveData();
+            log.info("所有数据解析完成！");
+            cachedDataList.clear();
+            cachedDataList = new LinkedList<>();
+        }
+
+        /**
+         * 加上存储数据库
+         */
+        private void saveData() {
+            log.info("读取{}条数据，开始处理！", cachedDataList.size());
+            recordsHandler.accept(cachedDataList);
+        }
+    }
+
+}
--- src/main/java/birdsnail/example/excel/ExcelTest.java
+++ src/main/java/birdsnail/example/excel/ExcelTest.java
@@ -1,20 +1,41 @@
 package birdsnail.example.excel;
 
 
-import com.alibaba.fastjson2.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Map;
+
+@Slf4j
 public class ExcelTest {
 
     public static void main(String[] args) {
-        JSONObject content = new JSONObject();
-        content.put("appName", "");
-        content.put("smsType", 11);
-        content.put("urlOfMsg", "hhht");
-        content.put("phone", "182756351");
+//        JSONObject content = new JSONObject();
+//        content.put("appName", "");
+//        content.put("smsType", 11);
+//        content.put("urlOfMsg", "hhht");
+//        content.put("phone", "182756351");
+//
+//        System.out.println(content.toString());
+//        System.out.println(content.toJSONString());
+//        System.out.println(JSONObject.toJSONString(content.toString()));
 
-        System.out.println(content.toString());
-        System.out.println(content.toJSONString());
-        System.out.println(JSONObject.toJSONString(content.toString()));
+        readExcel();
+    }
+
+    private static void readExcel() {
+        String file = "C:\\Users\\huadong.yang\\Desktop\\user_code.xlsx";
+        ExcelNoModelDataHandler.handleAll(file, list -> {
+            for (Map<Integer, String> map : list) {
+                String code = map.get(0);
+                String name = map.get(1);
+                String source = code.startsWith("ZHZC") ? "80026" : "80027";
+                if (code.startsWith("ZDSM")) {
+                    source = "";
+                }
+                log.info("INSERT INTO ips_crm_db.crm_lookup_item (classify_code, item_code, item_name, item_attr1, item_attr2,  status) " +
+                        "VALUES ( 'user_behavior_clue_config', '{}', '{}', '{}','{}', 0);", code, name, 3, source);
+            }
+        });
     }
 
 
